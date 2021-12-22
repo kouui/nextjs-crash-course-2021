2676101712bd2fde4dcadc1a2a98b066a93501e8
kouui <lelouchkako@gmail.com>
2021-12-22 23:19:01 +0900

    creating wrapper of pages

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div >
+      <Head>
+        <title>WebDev Newz</title>
+      </Head>
+
       <h1>Welcome</h1>
     </div>
   )
